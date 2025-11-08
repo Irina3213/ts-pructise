b2d730fefd4ac10cc2c5498d94905595370b6534
Iryna Yeremizina <yeremizina1@gmail.com>
2025-11-08 18:43:33 +0200

    chenged task 5

--- lessons react/src/1-module/05-functions.ts
+++ lessons react/src/1-module/05-functions.ts
@@ -1,26 +1,139 @@
+// /**
+//  * Типізація функцій
+//  * Типізація аргументів
+//  * Тип значення, яке повертає функція
+//  * Опціональні параметри
+//  */
+
+// function sum(x: number, y: number): number {
+//   const result = x + y;
+//   return result;
+// }
+// sum(1, 2);
+// //sum(1, '2'); Буде помилка, бо  передаємо другим аргументом рядок
+
+// // function makeMessage(word: string): void{
+// // console.log(word);
+// // }
+// // makeMessage('Hello');
+
+// export default function makeMessage(age: string | number): void {
+//   console.log(`Your age is ${age}`);
+// }
+// makeMessage("20");
+
+// function test(num: number): string {
+//   let value = "test";
+//   let result = value + num;
+//   return result;
+// }
+// const a = Number("6");
+// test(a);
+
+// interface StudentType {
+//   studentName: string;
+//   course: string;
+//   time: number;
+// }
+
+// const students: StudentType[] = [
+//   {
+//     studentName: "Olha",
+//     course: "JS",
+//     time: 8,
+//   },
+//   {
+//     studentName: "Oleg",
+//     course: "React",
+//     time: 8,
+//   },
+//   {
+//     studentName: "Tetiana",
+//     course: "HTML/CSS",
+//     time: 3,
+//   },
+// ];
+
+// function showCourses(studentsArr: StudentType[]): string[] {
+//   const coursesList = studentsArr.map((student) => student.course);
+//   return coursesList;
+// }
+// console.log(showCourses(students));
+
+// //напишемо функцію з опціональними параметрами
+// function makeMesage(username: string, login?: string): string | number {
+//   if (login !== undefined) {
+//     return `${username} has login ${login}`;
+//   } else {
+//     return `${username}`;
+//   }
+// }
+// makeMesage("Oleg", "test");
+// makeMesage("Tetiana");
+
+// //типізація методів обєктів
+// interface ProductType {
+//   title: string;
+//   price: number;
+// }
+// interface BasketType {
+//   owner: string;
+//   products: [] | ProductType[];
+//   showOwner: (x: number, y?: string) => string;
+//   addProduct: (product: ProductType) => void;
+// }
+// const basket: BasketType = {
+//   owner: "Oleg",
+//   products: [],
+//   // showOwner: ()=>{}
+//   showOwner(x: number) {
+//     return `The owner of basket is ${this.owner} and has ${x} products`;
+//   },
+//   addProduct(product: ProductType) {
+//     this.products.push(product); //-poki vidae pomilky
+//     // return this.products;
+//   },
+// };
+
+// basket.showOwner(5);
+// const avocado: ProductType = {
+//   title: "avocado",
+//   price: 100,
+// };
+// basket.addProduct(avocado);
+
 /**
- * Типізація функцій
- * Типізація аргументів
- * Тип значення, яке повертає функція
- * Опціональні параметри
+ * - Типізація функцій
+ * - Типізація аргументів
+ * - Тип значення, яке повертає функція
+ * - Опціональні параметри
  */
 
 function sum(x: number, y: number): number {
   const result = x + y;
   return result;
 }
+
 sum(1, 2);
-//sum(1, '2'); Буде помилка, бо  передаємо другим аргументом рядок
+// sum(1, "2"); буде помилка, бо передаємо другим аргументом рядок
 
-// function makeMessage(word: string): void{
-// console.log(word);
+// -------------------------------------------------------------------
+
+// function makeMessage(word: string): void {
+//     console.log(word);
+
 // }
-// makeMessage('Hello');
 
-export default function makeMessage(age: string | number): void {
-  console.log(`Your age is ${age}`);
-}
-makeMessage("20");
+// makeMessage("Hello")
+
+// export default function makeMessage(age: string | number): void {
+//     console.log(`Your age is ${age}`);
+
+// }
+
+// makeMessage(20)
+
+// ---------------------------------------------------------------------
 
 function test(num: number): string {
   let value = "test";
@@ -29,6 +142,8 @@ function test(num: number): string {
 }
 const a = Number("6");
 test(a);
+
+// ----------------------------------------------------------------------
 
 interface StudentType {
   studentName: string;
@@ -58,45 +173,52 @@ function showCourses(studentsArr: StudentType[]): string[] {
   const coursesList = studentsArr.map((student) => student.course);
   return coursesList;
 }
+
 console.log(showCourses(students));
 
-//напишемо функцію з опціональними параметрами
-function makeMesage(username: string, login?: string): string | number {
+// напишемо функцію з опціональними параметрами
+function makeMessage(username: string, login?: string): string | number {
   if (login !== undefined) {
     return `${username} has login ${login}`;
   } else {
-    return `${username}`;
+    return `${username} `;
   }
 }
-makeMesage("Oleg", "test");
-makeMesage("Tetiana");
 
-//типізація методів обєктів
+makeMessage("Oleg", "test");
+makeMessage("Tetiana", undefined);
+
+// -------------------------------------------------------
+
 interface ProductType {
   title: string;
   price: number;
 }
+
 interface BasketType {
   owner: string;
-  products: [] | ProductType[];
+  products: ProductType[];
   showOwner: (x: number, y?: string) => string;
   addProduct: (product: ProductType) => void;
 }
+
+// типізація методів обʼєктів
 const basket: BasketType = {
   owner: "Oleg",
   products: [],
-  // showOwner: ()=>{}
-  showOwner(x: number) {
+  // showOwner: () => {}
+  showOwner(x) {
     return `The owner of basket is ${this.owner} and has ${x} products`;
   },
   addProduct(product: ProductType) {
-    // this.products.push(product);-poki vidae pomilky
-    return this.products;
+    this.products.push(product);
+    // return this.products
   },
 };
 
 basket.showOwner(5);
-const avocado = {
+
+const avocado: ProductType = {
   title: "avocado",
   price: 100,
 };
